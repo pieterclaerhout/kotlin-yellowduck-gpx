99d1086e875eb7d58a3b7f35cc3f3a0de5a34f04
Pieter Claerhout <pieter@yellowduck.be>
2021-05-20 08:19:25 +0200

    Updated the tests

--- src/test/kotlin/be/yellowduck/gpx/DistanceTests.kt
+++ src/test/kotlin/be/yellowduck/gpx/DistanceTests.kt
@@ -2,6 +2,7 @@ package be.yellowduck.gpx
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.text.DecimalFormat
 
 class DistanceTests {
 
@@ -10,11 +11,13 @@ class DistanceTests {
 
         val dist = Distance(123456.0)
 
+        val separator = DecimalFormat().decimalFormatSymbols.decimalSeparator
+
         assertThat(dist.meters).isEqualTo(123456.0)
         assertThat(dist.kilometers).isEqualTo(123.456)
-        assertThat(dist.formattedKilometers).isEqualTo("123,46 km")
+        assertThat(dist.formattedKilometers).isEqualTo("123${separator}46 km")
         assertThat(dist.miles).isEqualTo(76.72840273461777)
-        assertThat(dist.formattedMiles).isEqualTo("76,73 mi")
+        assertThat(dist.formattedMiles).isEqualTo("76${separator}73 mi")
 
     }
 
